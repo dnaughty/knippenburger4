3ec81cc56d15134811123227777499824772a85f
Denis Naughton <mjolinor75@hotmail.com>
2020-11-02 00:32:59 -0500

    added bootstrap

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
 # Load the Rails application.
 require_relative 'application'
 
+const webpack = require('webpack')
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
 # Initialize the Rails application.
 Rails.application.initialize!
